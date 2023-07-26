aa6c2ab4289822c29905bf7691f5ce18b714a4fa
sangyeon <sangyeun33@gmail.com>
2023-07-26 17:04:19 +0900

    feat: SlideListSideBarView에 슬라이드 목록을 나타내는 slideTableView 구현

--- MyKeynote/MyKeynote/Source/View/SlideListSideBarView.swift
+++ MyKeynote/MyKeynote/Source/View/SlideListSideBarView.swift
@@ -11,6 +11,10 @@ protocol SlideListSideBarViewDelegate: AnyObject {
     func slideAddButtonDidTapped()
 }
 
+protocol SlideListSideBarViewDataSource: AnyObject {
+    func numberOfSlides() -> Int
+}
+
 final class SlideListSideBarView: UIView {
     
     enum Constants {
@@ -18,9 +22,12 @@ final class SlideListSideBarView: UIView {
         static let addButtonHeight = 40.0
     }
     
+    private let slideTableView = UITableView()
+    
     private let slideAddButton = UIButton()
     
     weak var delegate: SlideListSideBarViewDelegate?
+    weak var dataSource: SlideListSideBarViewDataSource?
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -40,17 +47,36 @@ final class SlideListSideBarView: UIView {
         translatesAutoresizingMaskIntoConstraints = false
         backgroundColor = .systemGray5
         
+        addSubviews()
+        setupConstraints()
+    }
+    
+    private func addSubviews() {
+        addSubview(slideTableView)
         addSubview(slideAddButton)
-        
+    }
+    
+    private func setupConstraints() {
         NSLayoutConstraint.activate([
             slideAddButton.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -Constants.inset),
             slideAddButton.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: Constants.inset),
             slideAddButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -Constants.inset),
             slideAddButton.heightAnchor.constraint(equalToConstant: Constants.addButtonHeight)
         ])
+        NSLayoutConstraint.activate([
+            slideTableView.topAnchor.constraint(equalTo: self.topAnchor, constant: Constants.inset),
+            slideTableView.leadingAnchor.constraint(equalTo: slideAddButton.leadingAnchor),
+            slideTableView.trailingAnchor.constraint(equalTo: slideAddButton.trailingAnchor),
+            slideTableView.bottomAnchor.constraint(equalTo: slideAddButton.topAnchor, constant: -Constants.inset)
+        ])
     }
     
     private func setupUIProperties() {
+        setupSlideAddButton()
+        setupSlideTableView()
+    }
+    
+    private func setupSlideAddButton() {
         slideAddButton.setTitle("+", for: .normal)
         slideAddButton.titleLabel?.font = .systemFont(ofSize: 30)
         slideAddButton.setTitleColor(.systemBlue, for: .normal)
@@ -61,7 +87,30 @@ final class SlideListSideBarView: UIView {
         slideAddButton.addTarget(self, action: #selector(slideAddButtonDidTapped(_:)), for: .touchUpInside)
     }
     
+    private func setupSlideTableView() {
+        slideTableView.translatesAutoresizingMaskIntoConstraints = false
+        slideTableView.dataSource = self
+        slideTableView.delegate = self
+        slideTableView.register(UITableViewCell.self, forCellReuseIdentifier: "UITableViewCell")
+        
+        slideTableView.backgroundColor = .yellow
+    }
+    
     @objc private func slideAddButtonDidTapped(_ sender: UIButton) {
         delegate?.slideAddButtonDidTapped()
     }
+}
+
+extension SlideListSideBarView: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
+    
 }
