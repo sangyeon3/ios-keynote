103e82d3373a6b827eb63625f46ba9f68e4ee2ff
sangyeon <sangyeun33@gmail.com>
2023-07-27 21:04:59 +0900

    feat: SlideManageable - Slide -> BaseSlide 타입 수정

--- MyKeynote/MyKeynote/Source/Model/Protocol/SlideManageable.swift
+++ MyKeynote/MyKeynote/Source/Model/Protocol/SlideManageable.swift
@@ -10,10 +10,9 @@ import Foundation
 protocol SlideManageable {
     var numberOfSlide: Int { get }
     
-    subscript(index: Int) -> Slide? { get }
-    func slide(havingID id: String) -> Slide?
-    @discardableResult
-    func addSlide<T: Slide>(type: T.Type) -> Slide
+    subscript(index: Int) -> BaseSlide? { get }
+    func slide(havingID id: String) -> BaseSlide?
+    func addSlide(_ newSlide: BaseSlide)
     func changeBackgroundColorOf(havingID slideID: String, to color: RGBColor)
     func changeAlphaOf(havingID slideID: String, to alphaValue: Int)
 }
