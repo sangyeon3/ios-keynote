43fb38d2267c64bfe01b3d172a4f324bbddb0f68
sangyeon <sangyeun33@gmail.com>
2023-07-28 02:02:49 +0900

    feat: PhotoSlideView 구현

---
+++ MyKeynote/MyKeynote/Source/View/Slide/PhotoSlideView.swift
@@ -0,0 +1,71 @@
+//
+//  PhotoSlideView.swift
+//  MyKeynote
+//
+//  Created by  sangyeon on 2023/07/28.
+//
+
+import UIKit
+
+final class PhotoSlideView: BaseSlideView, SlideViewImageChangeable, SlideViewAlphaChangeable {
+    
+    enum Constants {
+        static let inset = 10.0
+    }
+
+    init(image: UIImage?) {
+        super.init(frame: .zero)
+        
+        setupViews(image: image)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupViews(image: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        setupViews(image: nil)
+    }
+    
+    func changeSlideImage(to image: UIImage) {
+        <#code#>
+    }
+    
+    func changeAlpha(to value: Int) {
+        <#code#>
+    }
+    
+    private func setupViews(image: UIImage?) {
+        backgroundColor = .white
+        translatesAutoresizingMaskIntoConstraints = false
+        clipsToBounds = true
+        
+        setupContentView(image: image)
+    }
+    
+    private func setupContentView(image: UIImage?) {
+        let image = image ?? .photoSlideIcon
+        contentView = UIImageView(image: image)
+        contentView?.contentMode = .scaleAspectFit
+        
+        guard let contentView else { return }
+        
+        addSubview(contentView)
+        NSLayoutConstraint.activate([
+            contentView.topAnchor.constraint(equalTo: self.topAnchor, constant: Constants.inset),
+//            contentView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: Constants.inset),
+//            contentView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -Constants.inset),
+            contentView.widthAnchor.constraint(equalToConstant: 100),
+            contentView.heightAnchor.constraint(equalToConstant: 100),
+            contentView.centerXAnchor.constraint(equalTo: self.centerXAnchor)
+        ])
+    }
+}
+
+fileprivate extension UIImage {
+    static let photoSlideIcon = UIImage(systemName: "photo")
+}
